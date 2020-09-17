d5330ce863dab9e5b0e500a93a9c6981852eb1b8
KaribelBT <karibel.barco@gmail.com>
2020-09-17 20:47:25 -0300

    routes folder

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const CSVtoJSON = require('csvtojson');
+const FileSystem = require('fs');
+
+const sequelize = require('./database/database.js');
+const posts = require('./database/models/posts.js');
+const categories = require('./database/models/categories.js');
+const associations = require('./database/models/associations.js');
+
+app.use(express.json());
+app.use(require('./routes/routes.js'))
+
+//inicia servidor
+app.listen(port, () => {
+    console.log(`app started in: http://localhost:${port}`);
+    sequelize.sync({ alter: true }).then(() => {
+        console.log("Database connected");
+    }).catch(error => {
+        console.log('An error has ocurred', error);
+    })
+});
+
+//inserta csv
+categories.findAll().then(existingCategories => {
+    try {
+        if (existingCategories.length === 0) {
+            CSVtoJSON().fromFile('./data_base/bulk_insert/categories.csv').then(categories => {
+                categories.map(category => {
+                    let query = sequelize.query(
+                        `INSERT INTO categories (id, name)
+                        VALUES (:id, :name)`,
+                        {
+                            replacements: {
+                                id: category.id,
+                                name: category.name
+                            }
+                        });
+                    return query.then(resp => {
+                        console.log('categories inserted')
+                    })
+                })
+            });
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
+posts.findAll().then(existingPosts => {
+    try {
+        if (existingPosts.length === 0) {
+            CSVtoJSON().fromFile('./data_base/bulk_insert/posts.csv').then(posts => {
+                posts.map(post => {
+                    let query = sequelize.query(
+                        `INSERT INTO posts (id, category_id, title, content, img_url, create_date, enable)
+                        VALUES (:id, :category_id, :title, :content, :img_url, :create_date, :enable)`,
+                        {
+                            replacements: {
+                                id: post.id,
+                                category_id: post.category_id,
+                                title: post.title,
+                                content: post.content,
+                                img_url: post.img_url,
+                                create_date: post.create_date,
+                                enable: post.enable
+                            }
+                        });
+                    return query.then(resp => {
+                        console.log('posts inserted')
+                    })
+                })
+            });
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
---
+++ routes/routes.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const router = express.Router();
+
+const sequelize = require('../database/database.js');
+const posts = require('../database/models/posts.js');
+const categories = require('../database/models/categories.js');
+const associations = require('../database/models/associations.js');
+
+const middlewares = require('../middlewares/posts_middlewares.js');
+let myMiddleware = new middlewares.Middlewares();
+
+//lista todos los posts que no hayan sido borrados
+router.get('/posts', async (req, res) => {
+    let postsListed = await posts.findAll({
+        attributes: ['id', 'title', 'img_url', 'create_date'],
+        where: { enable: true },
+        order: [['create_date', 'DESC']],
+        include: [{
+            model: categories
+        }]
+    })
+    res.json(postsListed);
+});
+
+//obtiene post por id
+router.get('/posts/:id', myMiddleware.postDeleted(posts), async (req, res) => {
+    try {
+        let post = await posts.findOne({
+            attributes: ['id', 'title', 'content', 'img_url', 'create_date'],
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: categories
+            }]
+        });
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ error: 'Post not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: 'Bad Request, invalid or missing input' })
+    }
+});
+
+//crea un post
+router.post('/posts', async (req, res) => {
+    const { category_id, title, content, img_url } = req.body;
+    try {
+        let create = await posts.create({
+            category_id: category_id,
+            title: title,
+            content: content,
+            img_url: img_url
+        })
+        res.status(201).json(create);
+    } catch (error) {
+        res.status(400).json({ error: 'Bad Request, invalid or missing input' });
+    }
+});
+
+//modifica datos de post por id
+router.patch('/posts/:id', myMiddleware.postNotFound(posts, categories), myMiddleware.postDeleted(posts), async (req, res) => {
+    const { category_id, title, content, img_url } = req.body;
+    if (!req.body) {
+        res.status(400).json({ error: 'Bad Request, invalid or missing input' })
+    } else {
+        let postUpdated = await posts.update({
+            category_id: category_id,
+            title: title,
+            content: content,
+            img_url: img_url
+        },
+            { where: { id: req.params.id } }
+        );
+        postUpdated = await posts.findOne({
+            attributes: ['id', 'title', 'content', 'img_url', 'create_date'],
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: categories
+            }]
+        });
+        res.status(200).json(postUpdated);
+    }
+});
+
+//borrado logico de post por id
+router.delete('/posts/:id', myMiddleware.postNotFound(posts, categories), myMiddleware.postDeleted(posts), async (req, res) => {
+    try {
+        await posts.update({
+            enable: false
+        },
+            { where: { id: req.params.id } }
+        );
+        res.status(200).json({ message: 'Success, post disabled' });
+    }
+    catch{
+        res.status(400).json({ error: 'Bad Request, invalid or missing input' })
+    }
+});
+module.exports = router;
